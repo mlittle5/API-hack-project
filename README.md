7bea551d227d08469983ee0a926e9b1294d97dd9
Morgan Little <mrgnlit@gmail.com>
2020-10-03 22:38:51 -0700

    changed looks and feel

--- README.md
+++ README.md
@@ -1,4 +1,7 @@
-Get some space, a mars and earth weather comparison app complete with a mars rover picture in the background.
+<!-- TODO https://guides.github.com/features/mastering-markdown/ read this! -->
+
+# Get some space 
+## A mars and earth weather comparison app 
 
 https://mlittle5.github.io/API-hack-project/
 
@@ -6,16 +9,16 @@ https://mlittle5.github.io/API-hack-project/
 This app shows you the weather on Mars and allow you to input a zip code to get the weather of that location. It displays the temperature in both Fahrenheit and Celsius. 
 
 
-Technology used:
+# Technology used:
 1. javascript
-2. jQuery
-3. HTML
-4. CSS
+1. jQuery
+1. HTML
+1. CSS
 
-API's used:
+# API's used:
 1. InSight: Mars Weather Service API from NASA
-    -gets the weather from mars
-2. Mars Rover Photos API from NASA 
-    -gets a photo from a rover
-3. Current Weather Data from Openweather
-    -get the weather based on the location of a zip code. 
+    1. gets the weather from mars
+1. Mars Rover Photos API from NASA 
+    1. gets a photo from a rover
+1. Current Weather Data from Openweather
+    1. get the weather based on the location of a zip code. 
--- app.js
+++ app.js
@@ -2,85 +2,95 @@
 const nasaApiKey = 'gzN2hjFJgD6G6XZwz9WvpCmSNctEOVvP30vrp3zj';
 const openApiKey = '6ab109b556728e98f5218756e57e51df';
 //// DATA COLLECTION
-function getMarsWeather() {
+
+function getWeather(cityZip) {
   fetch(`https://api.nasa.gov/insight_weather/?api_key=${nasaApiKey}&feedtype=json&ver=1.0`)
     .then(response => response.json())
     .then(nasaData => {
-      displayMarsWeather(nasaData);
-      //displayMoreData(nasaData);
+      fetch(`https://api.openweathermap.org/data/2.5/weather?zip=${cityZip}&appid=${openApiKey}&units=metric`)
+        .then(response2 => response2.json())
+        .then(openData => {
+          console.log(nasaData, openData)
+          displayWeather(nasaData, openData)
+        })
+        .catch(err => console.error(err));
     })
     .catch(err => console.error(err));
+}
+//// Template Functions ////
+
+function weatherTemplate(marsWeather, marsWeatherConvert, earthWeather, earthWeatherConvert, cityZip) {
+  console.log("this worked");
+  return `
+  <h1 class="weather">It is ${marsWeather}&#8451; or ${marsWeatherConvert}&#8457; on Mars 
+  and<br>it is ${earthWeather}&#8451; or ${earthWeatherConvert}&#8457; in ${cityZip}</h1>
+  `
 }
 
-function getOpenWeather(cityZip) {
-  fetch(`https://api.openweathermap.org/data/2.5/weather?zip=${cityZip}&appid=${openApiKey}&units=metric`)
-    .then(response2 => response2.json())
-    .then(openData => {
-      displayEarthWeather(openData)
-    })
-    .catch(err => console.error(err));
+function hotTemplate() {
+  return `
+<h2> Wow, its hot on mars!</h2>
+`
+}
+function earthWeatherConvert() {
+
 }
 
-function getBackground() {
-  let url = `https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?sol=1000&api_key=${nasaApiKey}`
-  fetch(url)
-    .then(response => {
-      return response.json();
-    })
-    .then(data => {
-      let goodPhotos = data.photos.filter(photo => {
-        if (photo.camera.name === "NAVCAM") {
-          return true;
-        }
-        return false;
-      });
-      let imgSrc = goodPhotos[Math.floor(Math.random() * goodPhotos.length)].img_src;
-      $('body').css("background-image", `url("${imgSrc}")`);
-      // Do stuff with data (this is the JSON from the response body)
-    })
-    .catch(err => console.error(err));
-}
 //// Render functions ////
-function displayMarsWeather(nasaData) {
-  let farenheit = Math.trunc(nasaData[650].AT.av) * 9 / 5 + 32;
-  $('#mars-weather').html(`<h1 class="button1">It is ${nasaData[656].AT.av}&#8451; or ${farenheit}&#8457; on Mars</h1><br><button type="button" id="more-info" class="button1">More information</button>`);
-  $('.mars-weather').fadeIn("slow", function () {
-    //complete
-  });
-  watchMoreInfo();
-}
-function displayEarthWeather(openData) {
+function displayWeather(nasaData, openData,) {
   let cityZip = $('#js-search-term').val();
-  let farenheit = Math.trunc(openData.main.temp) * 9 / 5 + 32;
-  console.log(openData);
-  //openWeatherData.push(`<p>${openData.main.temp}</p>`);
-  $('#earth-weather').html(`<h1 class="button1">It is ${Math.trunc(openData.main.temp)}&#8451; or ${farenheit}&#8457; in ${cityZip}</h1>`);
-  $('.earth-weather').fadeIn("slow", function () {
-    //complete
-  });
-}
-function displayMoreData() {
-  $('.more-data').html(`<div><p class="button1">This weather data is provided by the InSight: Mars Weather Service API</p>
-  <br>
-  <p class="button1"> To learn more, click <a target="_blank" href="https://api.nasa.gov/">here</a></p>
-  </div>`);
+  let marsWeatherConvert = Math.trunc(nasaData[657].AT.av) * 9 / 5 + 32;
+  let earthWeatherConvert = Math.trunc(openData.main.temp) * 9 / 5 + 32;
+  let marsWeather = nasaData[656].AT.av;
+  let earthWeather = openData.main.temp;
+  $('.mars-weather').html(weatherTemplate(marsWeatherConvert, earthWeatherConvert, marsWeather, earthWeather, cityZip));
+  // if (marsWeatherConvert > 80) {
+  //   $('#mars-weather').html(hotTemplate());
+  // }
 
 }
+
+
+// function displayMarsWeather(nasaData) {
+//   let farenheit = Math.trunc(nasaData[650].AT.av) * 9 / 5 + 32;
+//$('#mars-weather').html(`<h1 class="button1">It is ${nasaData[656].AT.av}&#8451; or ${farenheit}&#8457; on Mars</h1><br><button type="button" id="more-info" class="button1">More information</button>`);
+// $('.mars-weather').fadeIn("slow", function () {
+//complete
+//});
+
+//   watchMoreInfo();
+// }
+// function displayEarthWeather(openData) {
+//   let cityZip = $('#js-search-term').val();
+//   let farenheit = Math.trunc(openData.main.temp) * 9 / 5 + 32;
+//   console.log(openData);
+//openWeatherData.push(`<p>${openData.main.temp}</p>`);
+//   $('#earth-weather').html(`<h1 class="button1">It is ${Math.trunc(openData.main.temp)}&#8451; or ${farenheit}&#8457; in ${cityZip}</h1>`);
+//   $('.earth-weather').fadeIn("slow", function () {
+//complete
+//   });
+// }
+// function displayMoreData() {
+//   $('.more-data').html(`<div><p class="button1">This weather data is provided by the InSight: Mars Weather Service API</p>
+//   <br>
+//   <p class="button1"> To learn more, click <a target="_blank" href="https://api.nasa.gov/">here</a></p>
+//   </div>`);
+
+// }
 //// Event Handlers ////
 function watchForm(openData) {
   $('#js-submit').submit(event => {
     event.preventDefault();
     const cityZip = $('#js-search-term').val();
-    getOpenWeather(cityZip);
+    getWeather(cityZip);
   });
 }
-function watchMarsButton() {
-  $('#mars-weather-button').click(event => {
-    event.preventDefault();
-    getMarsWeather();
-  })
-}
-
+// function watchMarsButton() {
+//   $('#mars-weather-button').click(event => {
+//     event.preventDefault();
+//     getMarsWeather();
+//   })
+// }
 
 function watchMoreInfo() {
   $('#more-info').click(event => {
@@ -90,12 +100,16 @@ function watchMoreInfo() {
   })
 }
 
+function refreshPage() {
+  //TODO make a thing to refresh the page
+}
+
 
 function renderPage() {
-  console.log('App loaded! Waiting for submit!');
+  //console.log('App loaded! Waiting for submit!');
   watchForm();
-  watchMarsButton();
+  //watchMarsButton();
   //watchMoreInfo();
 }
-$(getBackground);
+
 $(renderPage);
